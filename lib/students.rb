c481a2ccaf4c198686b0dc3ee3a2e552346081bc
Warda Ayaz <wayaz107@gmail.com>
2020-09-04 05:10:46 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,10 +9,11 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  "SELECT "
+  "SELECT AVG(gpa) FROM students"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
